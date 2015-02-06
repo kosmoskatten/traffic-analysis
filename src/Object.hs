68f2900dd77ca36b488d40558426188cd469233a
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-02-06 15:35:43 +0100

    Added the functionality field.

--- src/Object.hs
+++ src/Object.hs
@@ -22,24 +22,30 @@ data Transport = TCP | UDP
     deriving Show
 
 -- | Application protocol.
-data Application = HTTP
+data Application = C2DM | HTTP | XMPP
+    deriving Show
+
+-- | Functionality description.
+data Functionality = Audio | FileDownload | IMAndPrecense | SocialNetworking
+                   | Video | WebBrowsing
     deriving Show
 
 -- | An object is the aggregation of data packets from a continous
 -- transmission flow of a single IP flow.
-data Object = Object
-              { timestamp :: !Float -- Start of the object's
-                                    -- transmission since measurement
-                                    -- start. Microsecond precision.
-              , duration  :: !Float -- In seconds.
-              , transport :: !Transport -- Transport protocol.
-              , ulPackets :: !Int -- Number of uplink packets.
-              , dlPackets :: !Int -- Number of downlink packets.
-              , ulBytes   :: !Integer -- Number of uplink bytes.
-              , dlBytes   :: !Integer -- Number of downlink bytes.
-              , flowId    :: !LBS.ByteString -- 64 bit flow id.
-              , application :: !(Maybe Application) -- Application protocol.
-              }
+data Object = 
+    Object { timestamp     :: !Float -- Start of the object's
+                                     -- transmission since measurement
+                                     -- start. Microsecond precision.
+           , duration      :: !Float -- In seconds.
+           , transport     :: !Transport -- Transport protocol.
+           , ulPackets     :: !Int -- Number of uplink packets.
+           , dlPackets     :: !Int -- Number of downlink packets.
+           , ulBytes       :: !Integer -- Number of uplink bytes.
+           , dlBytes       :: !Integer -- Number of downlink bytes.
+           , flowId        :: !LBS.ByteString -- 64 bit flow id.
+           , application   :: !(Maybe Application) -- Application protocol.
+           , functionality :: !(Maybe Functionality) -- Functionality.
+           }
     deriving Show
 
 instance FromField Transport where
@@ -48,12 +54,23 @@ instance FromField Transport where
   parseField _     = mzero
 
 instance FromField Application where
-  parseField "HTTP" = return HTTP
-  parseField _      = mzero
+  parseField "Android C2DM" = return C2DM
+  parseField "HTTP"         = return HTTP
+  parseField "XMPP"         = return XMPP
+  parseField _              = mzero
+
+instance FromField Functionality where
+  parseField "audio"             = return Audio
+  parseField "file-download"     = return FileDownload
+  parseField "IM and presence"   = return IMAndPrecense
+  parseField "social networking" = return SocialNetworking
+  parseField "video"             = return Video
+  parseField "web browsing"      = return WebBrowsing
+  parseField _                   = mzero
 
 instance FromRecord Object where
   parseRecord v
-    | V.length v == 9 = Object <$> v .! 0
+    | V.length v == 10 = Object <$> v .! 0
                                <*> v .! 1
                                <*> v .! 2
                                <*> v .! 3
@@ -62,6 +79,7 @@ instance FromRecord Object where
                                <*> v .! 6
                                <*> v .! 7
                                <*> v .! 8
+                               <*> v .! 9
     | otherwise       = mzero
 
 -- | Decode the bytestring for objects. The objects are expected to be
