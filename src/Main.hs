42368f3ce8078b5b4a97a73ac5a7349264e5344a
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-02-08 23:22:25 +0100

    Some performance testing code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Control.DeepSeq
+import qualified Data.ByteString.Lazy as LBS
+import Network.Traffic.Object
+
 main :: IO ()
-main = return ()
+main = do
+  file <- LBS.readFile "long.csv"
+  file `deepseq` putStrLn "File read"
+  let result = decodeObjects file
+  case result of
+    Right objects -> print (enumerateByApplication objects)
+    Left err      -> print err
