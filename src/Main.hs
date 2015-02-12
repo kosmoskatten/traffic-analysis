bf36220bd16ef17a029f35f7cac1d65bc9e0f7f4
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-02-12 16:31:35 +0100

    Start quantifying enumerations.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
 import Network.Traffic.Object
 import System.Environment (getArgs)
@@ -16,6 +17,16 @@ analyzeObjects :: ObjectVector -> IO ()
 analyzeObjects objects = do
   putStrLn $ "Length is: " ++ show (V.length objects)
   putStrLn $ "---------- enumerateByTransport"
-  print (enumerateByTransport objects)
+  let trEnum = enumerateByTransport objects
+  print (quantifyEnumeration trEnum)
+  print trEnum
+  
   putStrLn $ "---------- enumerateByApplication"
-  print (enumerateByApplication objects)
+  let appEnum = enumerateByApplication objects
+  print (quantifyEnumeration appEnum)
+  print appEnum
+
+quantifyEnumeration :: Show a => M.Map a Int -> (Int, [(String, Int, Float)])
+quantifyEnumeration enumeration =
+    let totalCount = M.foldl' (+) 0 enumeration
+    in (totalCount, [])
