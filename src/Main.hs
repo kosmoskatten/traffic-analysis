00ec4932e369f9ae44b6bc2033a6ebce088d483f
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-02-14 22:38:22 +0100

    Added a second ticker indicator while waiting for analysis.

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,25 @@
 module Main where
 
-import Control.Monad (forM_)
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (async, cancel)
+import Control.Monad (forM_, forever)
 import qualified Data.ByteString.Lazy as LBS
 import Network.Traffic.Object
 import System.Environment (getArgs)
+import System.IO (hPutChar, hFlush, stdout)
 import Text.Printf (printf)
 
 main :: IO ()
 main = do
+  t    <- async ticker
   file <- LBS.readFile =<< head `fmap` getArgs
   case decodeObjectsPar file of
-    Right objects -> analyzeObjects objects                
-    Left err      -> print err
+    Right objects -> do
+      cancel t
+      analyzeObjects objects                
+    Left err      -> do
+      cancel t
+      print err
 
 analyzeObjects :: ObjectVector -> IO ()
 analyzeObjects objects = do
@@ -22,6 +30,14 @@ analyzeObjects objects = do
 
 printQuantification :: String -> Quantification -> IO ()
 printQuantification label (total, quantList) = do
-  printf "Quantification of %s. Total # of items: %ld\n" label total
+  printf "\nQuantification of %s. Total # of items: %ld\n" label total
   forM_ quantList $ \(name, _, perc) ->
     printf " Item %s has %.2f percent.\n" (show name) perc
+
+ticker :: IO ()
+ticker =
+  forever $ do
+    threadDelay 1000000
+    hPutChar stdout '.'
+    hFlush stdout
+
