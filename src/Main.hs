6545c402ea5249de9407c40f75174c37511a6660
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-02-14 22:20:58 +0100

    Better printing of quantifications.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Monad (forM_)
 import qualified Data.ByteString.Lazy as LBS
 import Network.Traffic.Object
 import System.Environment (getArgs)
+import Text.Printf (printf)
 
 main :: IO ()
 main = do
@@ -13,10 +15,13 @@ main = do
 
 analyzeObjects :: ObjectVector -> IO ()
 analyzeObjects objects = do
-  putStrLn $ "---------- enumerateByTransport"
-  let trEnum = enumerateByTransport objects
-  print (quantifyEnumeration trEnum)
-  
-  putStrLn $ "---------- enumerateByApplication"
-  let appEnum = enumerateByApplication objects
-  print (quantifyEnumeration appEnum)
+  printQuantification "transport protocols" $
+    quantifyEnumeration (enumerateByTransport objects)
+  printQuantification "application protocols" $
+    quantifyEnumeration (enumerateByApplication objects)
+
+printQuantification :: String -> Quantification -> IO ()
+printQuantification label (total, quantList) = do
+  printf "Quantification of %s. Total # of items: %ld\n" label total
+  forM_ quantList $ \(name, _, perc) ->
+    printf " Item %s has %.2f percent.\n" (show name) perc
